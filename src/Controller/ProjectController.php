16c76f780fe52625f06b0364f6166f9f256478db
otdot <s2200126@BC-C02GR0BSQ6LR-MacBook-Air.local>
2022-05-18 14:49:23 +0300

    minor fixes

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -12,7 +12,7 @@ class ProjectController extends AbstractController
     public function index(): Response
     {
 
-        $profiles = [["name" => "Ebi", "image" => "http://source.unsplash.com/9SSJx7m_Tbs"], 
+        $profiles = [["name" => "Ebi", "image" => "https://i.postimg.cc/RhLc7ytX/8692-EE71-DC71-46-A5-B08-E-CF01-B1-FE0-C08.jpg"], 
                      ["name" => "Filipp", "image" => "https://scontent-arn2-1.xx.fbcdn.net/v/t1.6435-9/36786402_2220990794797480_8918722842544046080_n.jpg?_nc_cat=101&ccb=1-6&_nc_sid=09cbfe&_nc_ohc=pRJiCCFU_KoAX8XW2Dv&_nc_ht=scontent-arn2-1.xx&oh=00_AT8NIJvS5h3RjaLWKmXSzsEczQrPIsbWMvSGmofFVPN69w&oe=62A5ED62"], 
                      ["name" => "Otto", "image" => "https://media-exp1.licdn.com/dms/image/C5603AQFeGQRi9rg14Q/profile-displayphoto-shrink_200_200/0/1567242235174?e=1657756800&v=beta&t=qoKcxZYG0H4nN7kVoQC62sL1VqNq_FBfNwRaGZzWkIo"], 
                      ["name" => "Toni", "image" => "https://avatars.githubusercontent.com/u/54765887?v=4"]];
